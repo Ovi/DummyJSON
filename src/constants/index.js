620c9b5de129cb5f8896448e0af53806922701e7
Erik Derohanian <erik@erik-j.de>
2023-06-02 13:28:07 -0400

    Update index.js with 418
    Add support for https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/418

--- src/constants/index.js
+++ src/constants/index.js
@@ -52,6 +52,7 @@ constants.httpCodes = {
   415: 'Unsupported Media Type',
   416: 'Requested Range Not Satisfiable',
   417: 'Expectation Failed',
+  418: "I'm a teapot",
   420: 'Enhance Your Calm',
   422: 'Unprocessable Entity',
   423: 'Locked',
