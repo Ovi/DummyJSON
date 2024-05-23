a38862801f687de2ec1186e02d105b0b99d98c94
Muhammad Ovi (Owais) <contact@muhammadovi.com>
2024-05-23 13:45:50 +0500

    [Generator] Quotes

---
+++ raw/quotes/quotes-data-generator.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+function generateData() {
+  try {
+    const data = fs.readFileSync('./raw/quotes/quotes.json', 'utf8');
+    const json = JSON.parse(data);
+
+    const newData = json.map((item, idx) => ({
+      id: idx + 1,
+      ...item,
+    }));
+
+    fs.writeFileSync('./database/quotes.json', JSON.stringify(newData, null, 2), 'utf8');
+    console.log('Data Generated Successfully!');
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+generateData();
