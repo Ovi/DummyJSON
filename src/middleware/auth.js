d6726da900554d305d3ef2ecbb0a7d8d997a6070
Muhammad Ovi (Owais) <contact@muhammadovi.com>
2024-08-10 01:48:38 +0500

    🍪 Add support for cookies

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,7 +4,12 @@ const { findUserWithUsernameAndId } = require('../utils/util');
 
 const authUser = async (req, res, next) => {
   try {
-    const token = req.header('Authorization');
+    const tokenFromHeader = req.header('Authorization');
+    const tokenFromCookie = req.cookies.accessToken;
+
+    // Prefer token from Authorization header if both are present
+    const token = tokenFromHeader || tokenFromCookie;
+
     if (!token) throw new APIError('Authentication Problem', 403);
 
     const decoded = await verifyAccessToken(token);
