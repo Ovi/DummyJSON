36f9f19ac55a69f047d855ce1fb93ddbb46ae8c6
Muhammad Ovi (Owais) <contact@muhammadovi.com>
2024-09-14 17:51:22 +0500

    🛠 Update db connection config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,14 +5,17 @@ const { MONGODB_URI, MONGODB_DB_NAME } = process.env;
 
 async function connectDB() {
   if (!MONGODB_URI || isDbConnected()) return;
+
   const dbOptions = {
     dbName: MONGODB_DB_NAME || 'DummyJSON-Test-DB',
+    maxPoolSize: 20,
+    serverSelectionTimeoutMS: 5000, // Time out after 5 seconds if no server is found
+    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
   };
 
   try {
     await mongoose.connect(MONGODB_URI, dbOptions);
     updatedDbConnectionStatus(true);
-    console.info('[Service:Database] Connected.');
   } catch (err) {
     updatedDbConnectionStatus(false);
     console.error('[Service:Database] Err: Failed to Connect.', err);
@@ -22,20 +25,31 @@ async function connectDB() {
 
 module.exports = connectDB;
 
-// If mongoose gets disconnected, show this message
+mongoose.connection.on('connected', () => {
+  console.info('[Service:Database] Mongoose connected to DB');
+});
+
+mongoose.connection.on('error', err => {
+  updatedDbConnectionStatus(true);
+  console.error('[Service:Database] Mongoose connection error:', err);
+});
+
 mongoose.connection.on('disconnected', () => {
   updatedDbConnectionStatus(false);
-  console.info('[Service:Database] Disconnected.');
+  console.info('[Service:Database] Mongoose disconnected from DB');
 
-  // [optional] exit app when database is disconnected
+  // exit app when database is disconnected
   // process.exit(1);
 });
 
 // If node exits, terminate mongoose connection
-process.on('SIGINT', () => {
-  mongoose.connection.close(() => {
-    console.info('INFO: Node is down. So the Mongoose.');
-
+process.on('SIGINT', async () => {
+  try {
+    await mongoose.connection.close();
+    console.info('INFO: Node is down. So is Mongoose.');
     process.exit(0);
-  });
+  } catch (err) {
+    console.error('Error during Mongoose disconnection:', err);
+    process.exit(1); // Exit with an error code if something goes wrong
+  }
 });
