552a5b1dc05850e8ee19145048487879e9dca97a
Muhammad Ovi (Owais) <contact@muhammadovi.com>
2025-09-16 17:50:07 +0500

    🛠 Revert change how `/ip` responds

--- src/routes/ip.js
+++ src/routes/ip.js
@@ -2,11 +2,7 @@ const router = require('express').Router();
 const extractClientInfo = require('../utils/client-info');
 
 router.get('/', (req, res) => {
-  res.send(extractClientInfo(req).ip);
-});
-
-router.get('/ua', (req, res) => {
-  res.send(extractClientInfo(req).userAgent);
+  res.send(extractClientInfo(req));
 });
 
 module.exports = router;
